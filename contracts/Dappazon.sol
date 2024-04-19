94a504b8226df608c45d1b53a2220e9605b27ddb
Mudita-Srivastava <muditasrivastava2020@gmail.com>
2024-04-19 13:56:04 +0530

    Initial commit

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,93 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+    mapping(uint256 => Item) public items;
+    //no of order of one person
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    constructor() {
+        owner=msg.sender;
+    }
+    event List(string name, uint256 cost, uint256 quantity);
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+
+modifier onlyOwner(){
+    require(msg.sender == owner);
+    _;
+}
+    //list products
+    function list(
+        uint256 _id,
+         string memory _name,
+          string memory _category,
+          string memory _image,
+          uint256 _cost,
+          uint256 _rating,
+          uint256 _stock
+          ) public onlyOwner{
+
+            
+         //cretae Item struct
+         Item memory item = Item(
+            _id, 
+            _name,
+             _category, 
+             _image, 
+             _cost, 
+             _rating,
+              _stock
+              );
+         //save Item struct to blockchain
+         items[_id]= item;
+         //emit an event
+         emit List(_name, _cost, _stock);
+    }
+
+    //buy products
+function buy(uint256 _id) public payable{
+    //receive crypto
+    //fetch item
+    Item memory item = items[_id];
+    //rnough ether to buy items
+    require(msg.value >= item.cost);
+    require(item.stock>0);
+
+    //create an order
+    Order memory order = Order(block.timestamp, item);
+    //save order to chain
+orderCount[msg.sender]++; //orderid
+orders[msg.sender][orderCount[msg.sender]]= order;
+
+    //substract stock
+    items[_id].stock= item.stock-1;
+
+    //emit event
+    emit Buy(msg.sender, orderCount[msg.sender], item.id);
+}
+
+    //withdraw funds from person who owns the market place
+function withdraw() public onlyOwner{
+    (bool success,)= owner.call{value: address(this).balance}("");
+    //It sends the entire balance (address(this).balance) of the current contract to the owner address.
+    require(success);
+}
+}
--- src/App.js
+++ src/App.js
@@ -13,12 +13,72 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const[provider, setProvider] = useState(null)
+  const[dappazon, setDappazon] = useState(null)
+  const[account, setAccount] = useState(null)
+  const[electronics, setElectronics] = useState(null)
+  const[clothing, setClothing]= useState(null)
+  const[toys, setToys] = useState(null)
+
+  const[item,setItem] = useState({})
+  const[toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
+const loadBlockchainData = async () => {
+  //connect to blockchain
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  setProvider(provider)
+
+  const network = await provider.getNetwork()
+  console.log(network)
+  //connect to smart contract (create js versions)
+  const dappazon = new ethers.Contract(
+    config[network.chainId].dappazon.address,
+     Dappazon,
+      provider)
+      setDappazon(dappazon)
+
+  //load products
+  const items=[]
+  for (var i=0; i<9; i++) {
+    const item= await dappazon.items(i+1)
+    items.push(item)
+  }
+  const electronics= items.filter((item) => item.category  === 'electronics')
+  const clothing = items.filter((item) => item.category === 'clothing')
+  const toys = items.filter((item) => item.category === 'toys')
+
+  const togglePop = (item) => {
+    setItem(item)
+  }
+
+  setElectronics(electronics)
+  setClothing(clothing)
+  setToys(toys)
+}
+
+useEffect(() => {
+  loadBlockchainData()
+},[])
 
   return (
     <div>
-
-      <h2>Welcome to Dappazon</h2>
-
+<Navigation account={account} setAccount={setAccount} />
+      <h2>Dappazon Best Sellers</h2>
+      {electronics && clothing && toys && (
+        <>
+        <Section title={"clothing & jewelry"} items={clothing} togglePop={togglePop} />
+        <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+        <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+      {toggle && (
+        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
+  )}
     </div>
   );
 }
